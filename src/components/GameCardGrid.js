840f810256e719133eed97e167ba02a43af4baff
Connor Bowling <46806071+ConnorB26@users.noreply.github.com>
2023-07-30 17:48:06 -0500

    Small code cleanup & navigation link changes

--- src/components/GameCardGrid.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './GameCardGrid.module.css';
-
-const GameCardGrid = ({ children }) => {
-  return <div className={styles.gameCardGrid}>{children}</div>;
-};
-
-export default GameCardGrid;
